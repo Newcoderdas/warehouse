a4d429ab8e200dc31c082b7be47e2cd217bcb138
Newcoderdas <106629183+Newcoderdas@users.noreply.github.com>
2024-11-24 08:15:14 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+import Header from "@/components/Header";
+import Hero from "@/components/Hero"
+import Card from "@/components/Card";
+import Image from "next/image";
+import Clients from "@/components/Clients";
+import OfferSection from "@/components/OfferSection";
+import Approach from "@/components/Approach";
+import ThreeImages from "@/components/ThreeImages";
+
+export default function Home() {
+  return (
+    <>
+    <Header/>
+    <Hero/>
+    <OfferSection/>
+    <Card/>
+    <Clients/>
+    <ThreeImages/>
+    <Approach/>
+    </>
+  );
+}
---
+++ src/components/Clients.tsx
@@ -0,0 +1,117 @@
+"use client";
+import React from "react";
+
+function Clients() {
+  return (
+    <>
+      <div className="max-w-[85rem] px-4 py-10 sm:px-6 lg:px-8 lg:py-14 mx-auto">
+        <div className="w-2/3 sm:w-1/2 lg:w-1/3 mx-auto text-center mb-6">
+          <h2 className="text-zinc-900 className:text-neutral-400 ">
+          Trusted by logistics professionals, businesses, and more than 99,000 warehouse operators worldwide.
+          </h2>
+        </div>
+
+        <div className="grid grid-cols-3 md:grid-cols-6 gap-x-6">
+          <img
+            className="py-3 lg:py-5 w-16 h-auto md:w-20 lg:w-24 mx-auto text-gray-500 className:text-neutral-500"
+            src="https://cdn.brandfetch.io/idv0ZbfQqf/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+          />
+           
+         
+
+          <img
+            className="py-3 lg:py-5 w-16 h-auto md:w-20 lg:w-24 mx-auto text-gray-500 className:text-neutral-500"
+            src="https://cdn.brandfetch.io/id6C1TEJxN/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+          />
+           
+          
+
+          <img
+            className="py-3 lg:py-5 w-16 h-auto md:w-20 lg:w-24 mx-auto text-gray-500 className:text-neutral-500"
+            src="https://cdn.brandfetch.io/id-ny4dYLQ/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+          />
+            
+
+          <img
+            className="py-3 lg:py-5 w-16 h-auto md:w-20 lg:w-24 mx-auto text-gray-500 className:text-neutral-500"
+            src="https://cdn.brandfetch.io/idkccMhoPZ/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+          />
+          
+          
+
+          <img
+            className="py-3 lg:py-5 w-16 h-auto md:w-20 lg:w-24 mx-auto text-gray-500 className:text-neutral-500"
+           src="https://cdn.brandfetch.io/idVIIZH-LB/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+          />
+            
+          
+         
+
+          <img
+            className="py-3 lg:py-5 w-16 h-auto md:w-20 lg:w-24 mx-auto text-gray-500 className:text-neutral-500"
+           src="https://cdn.brandfetch.io/id6v7qnJfo/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+          />
+           
+        </div>
+
+        <div className="grid grid-cols-3 md:grid-cols-5 gap-x-6 sm:gap-x-6">
+          <img
+            className="py-3 lg:py-5 w-16 h-auto md:w-20 lg:w-24 mx-auto text-gray-500 className:text-neutral-500"
+            src="https://cdn.brandfetch.io/idvUUwP3ul/w/1900/h/447/theme/dark/logo.png?c=1dxbfHSJFAPEGdCLU4o5B"
+          />
+            
+         
+
+          <img
+            className="py-3 lg:py-5 w-16 h-auto md:w-20 lg:w-24 mx-auto text-gray-500 className:text-neutral-500"
+            src="https://cdn.brandfetch.io/id38VKxYqy/w/518/h/192/theme/dark/logo.png?c=1dxbfHSJFAPEGdCLU4o5B"
+          />
+        
+
+          <img
+            className="py-3 lg:py-5 w-16 h-auto md:w-20 lg:w-24 mx-auto text-gray-500 className:text-neutral-500"
+            src="https://cdn.brandfetch.io/idWK5t6PG_/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+          />
+         
+
+          <img
+            className="py-3 lg:py-5 w-16 h-auto md:w-20 lg:w-24 mx-auto text-gray-500 className:text-neutral-500"
+           src="https://cdn.brandfetch.io/idUYOLMr8D/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+          />
+        
+
+          <img
+            className="py-3 lg:py-5 w-16 h-auto md:w-20 lg:w-24 mx-auto text-gray-500 className:text-neutral-500"
+            src="https://cdn.brandfetch.io/idUL5d1fhA/theme/dark/logo.svg?c=1dxbfHSJFAPEGdCLU4o5B"
+          />
+          
+        </div>
+
+        {/* <div className="mt-8 text-center">
+          <a
+            className="py-3 px-4 inline-flex items-center gap-x-2 text-sm font-medium rounded-full border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none className:bg-neutral-900 className:border-neutral-700 className:text-white className:hover:bg-neutral-800 className:focus:bg-neutral-800"
+            href="#"
+          >
+            Read case studies
+            <svg
+              className="shrink-0 size-4"
+              xmlns="http://www.w3.org/2000/svg"
+              width="24"
+              height="24"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path d="m9 18 6-6-6-6" />
+            </svg>
+          </a>
+        </div> */}
+      </div>
+    </>
+  );
+}
+
+export default Clients;
